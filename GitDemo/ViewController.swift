85e8a492dd80112c48fcc776bfc3087f886313c2
Gabriel Nadel <gabriel.nadel@gmail.com>
2015-12-30 11:03:24 -0800

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Gabriel Nadel on 12/30/15.
+//  Copyright © 2015 Gabriel Nadel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
